ecf8207343c18e614ae3d0d5ed185ec3131d42b6
BustamanteFlocer <77340285+BustamanteFlocer@users.noreply.github.com>
2024-04-02 09:59:47 +0800

    edit

--- src/main/java/ph/com/paraiso/controller/BookingController.java
+++ src/main/java/ph/com/paraiso/controller/BookingController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/user")
 public class BookingController {
 	@GetMapping("/login")
 	public String loginPage() {
