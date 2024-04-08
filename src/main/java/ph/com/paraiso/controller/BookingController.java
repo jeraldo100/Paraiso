a1ea7916564431be2664ea8fbc704d26d5ddc267
AmiiiielPauuuul <152834224+AmiiiielPauuuul@users.noreply.github.com>
2024-04-08 10:15:53 +0800

    BOOKING CONTROLLER UPDATE

--- src/main/java/ph/com/paraiso/controller/BookingController.java
+++ src/main/java/ph/com/paraiso/controller/BookingController.java
@@ -1,6 +1,6 @@
 package ph.com.paraiso.controller;
 
-import java.sql.Date;
+import java.util.Date;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,20 +17,33 @@ import ph.com.paraiso.service.BookingService;
 import ph.com.paraiso.service.RoomService;
 import ph.com.paraiso.service.UserService;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.concurrent.TimeUnit;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+
 @Controller
 public class BookingController {
 	
-	private BookingService bookingService;
+	private BookingService bookServ;
 	
 	public BookingController(BookingService bookingService) {
 		super();
-		this.bookingService = bookingService;
+		this.bookServ = bookingService;
 	}
 
 	
 	@GetMapping("/AdminBooking")
 	public String adminBooking(Model model) {
-		model.addAttribute("bookings", bookingService.getAllBookings());
+		model.addAttribute("bookings", bookServ.getAllBookings());
 		return "dashboardAdmin/Booking";
 	}
 	
@@ -68,14 +81,14 @@ public class BookingController {
 		booking.setChildren(children);
 		booking.setStatus(status);
 		
-		bookingService.addBooking(booking);
+		bookServ.addBooking(booking);
 		
 		return "redirect:/AdminBooking";
 	}
 	
 	@GetMapping("/editBooking/{booking_id}")
 	public String editBooking(@PathVariable("booking_id") Integer booking_id, Model model) {
-		model.addAttribute("booking", bookingService.getBookingById(booking_id));
+		model.addAttribute("booking", bookServ.getBookingById(booking_id));
 		return "dashboardAdmin/BookingCRUD/EditBooking";
 	}
 	
@@ -84,7 +97,7 @@ public class BookingController {
 			@ModelAttribute("booking") Booking booking,
 			Model model) {
 		
-		Booking existingBooking = bookingService.getBookingById(booking_id);
+		Booking existingBooking = bookServ.getBookingById(booking_id);
 		existingBooking.setBooking_id(booking_id);
 		existingBooking.setUser_id(booking.getUser_id());
 		existingBooking.setCheckin_date(booking.getCheckin_date());
@@ -95,7 +108,7 @@ public class BookingController {
 		existingBooking.setChildren(booking.getChildren());
 		existingBooking.setStatus(booking.getStatus());
 		
-		bookingService.updateBooking(existingBooking);
+		bookServ.updateBooking(existingBooking);
 		return "redirect:/AdminBooking";
 		
 		
@@ -104,7 +117,7 @@ public class BookingController {
 	@GetMapping("/deleteBooking/{booking_id}")
 	public String deleteBooking(@PathVariable("booking_id") Integer booking_id) {
 		
-		bookingService.deleteById(booking_id);
+		bookServ.deleteById(booking_id);
 		return "redirect:/AdminBooking";
 		
 		
@@ -122,10 +135,10 @@ public class BookingController {
 
 	@PostMapping("/updateStatus/{bookingId}")
 	public ResponseEntity<String> updateStatus(@PathVariable("bookingId") Integer bookingId, @RequestParam("status") String status) {
-	    Booking booking = bookingService.getBookingById(bookingId);
+	    Booking booking = bookServ.getBookingById(bookingId);
 	    if (booking != null) {
 	        booking.setStatus(status);
-	        bookingService.updateBooking(booking);
+	        bookServ.updateBooking(booking);
 	        return new ResponseEntity<>("Status updated successfully", HttpStatus.OK);
 	    } else {
 	        return new ResponseEntity<>("Booking not found", HttpStatus.NOT_FOUND);
@@ -133,5 +146,29 @@ public class BookingController {
 	    
 	    
 	}
+	
+	@GetMapping("/booking")
+	public String bookingPage(Model model) throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		double diff = Math.abs( (sdf.parse(sdf.format(new Date())).getTime()) - (sdf.parse(sdf.format(new Date())).getTime()) );
+		
+		model.addAttribute("days", TimeUnit.DAYS.convert( (long) diff, TimeUnit.MILLISECONDS) );
+		model.addAttribute("checkin_date" , sdf.format(new Date()));
+		model.addAttribute("checkout_date" , sdf.format(new Date()));
+		model.addAttribute("room_types",bookServ.listAllRoom_type(sdf.format(new Date()), sdf.format(new Date()) ) );
+		return "booking/Booking";
+	}
+	
+	@PostMapping("/checkAvailability")
+	public String checkAvailability(@RequestParam String checkin_date, String checkout_date, Model model) throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		double diff = Math.abs( (sdf.parse(checkin_date).getTime()) - (sdf.parse(checkout_date).getTime()) );
+		
+		model.addAttribute("days", TimeUnit.DAYS.convert( (long) diff, TimeUnit.MILLISECONDS) );
+		model.addAttribute("checkin_date", checkin_date);
+		model.addAttribute("checkout_date", checkout_date);
+		model.addAttribute("room_types",bookServ.listAllRoom_type(checkin_date, checkout_date));
+		return "booking/Booking";
+
 	
 }
