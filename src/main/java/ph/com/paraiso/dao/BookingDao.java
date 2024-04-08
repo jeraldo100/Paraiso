7e266b7f87f5fc9cda04a5bc93b9f4a874a32d79
AmiiiielPauuuul <152834224+AmiiiielPauuuul@users.noreply.github.com>
2024-04-08 10:19:02 +0800

    Booking Dao updated

--- src/main/java/ph/com/paraiso/dao/BookingDao.java
+++ src/main/java/ph/com/paraiso/dao/BookingDao.java
@@ -1,9 +1,13 @@
 package ph.com.paraiso.dao;
 
+import java.util.List;
+
 import ph.com.paraiso.model.Booking;
+import ph.com.paraiso.model.Room_type;
 
 public interface BookingDao {
 
+	List<Room_type> listAllRoom_type(String checkin_date, String checkout_date);
 	
 	public Booking addBooking(Booking booking);
 	
