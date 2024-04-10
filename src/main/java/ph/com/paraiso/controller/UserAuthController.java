90d254a8f601e8118cd9e840ac7bd03740349948
BustamanteFlocer <77340285+BustamanteFlocer@users.noreply.github.com>
2024-04-10 13:30:45 +0800

    deleted some controllers

--- src/main/java/ph/com/paraiso/controller/UserAuthController.java
+++ src/main/java/ph/com/paraiso/controller/UserAuthController.java
@@ -14,11 +14,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import ph.com.paraiso.dto.UserDto;
-import ph.com.paraiso.model.Booking;
 import ph.com.paraiso.model.User;
-import ph.com.paraiso.service.BookingService;
 import ph.com.paraiso.service.UserService;
 import ph.com.paraiso.session.SessionManager;
 
